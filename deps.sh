fe3be69ad721688d6f782c830763d7dec4425baa
hayde <haydenhowell75@gmail.com>
2025-05-16 10:52:26 -0400

    DEFAULT START

--- deps.sh
+++ deps.sh
@@ -1,4 +1,4 @@
-sudo dnf install gtk4 gtk4-devel cmake gcc pkgconf-pkg-config libcurl-devel sqlite sqlite-devel
+sudo dnf install gtk4 gtk4-devel cmake gcc g++ pkgconf-pkg-config libcurl-devel sqlite sqlite-devel
 
 
 
